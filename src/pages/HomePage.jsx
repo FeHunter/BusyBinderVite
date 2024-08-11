c5346d6687a81ecf1b3734ec1d3afd5a4e6ec84d
FeHunter <hunterfe1111@gmail.com>
2024-08-11 20:03:53 -0300

    SliderShow for content and products

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
+import Slider from "react-slick";
 import titleText, { contentText } from "../assets/SiteText";
 import { Header } from "../components/Header/Header";
 import { ImagesContent } from "../components/ImagesContent/ImagesContent";
 import { PageTitle } from "../components/PageTitle/PageTitle";
 import { ProdcutCard } from "../components/ProdcutCard/ProdcutCard";
 import style from "./HomePage.module.css";
+import { color } from "framer-motion";
 
 export function HomePage (){
     return (
@@ -25,6 +27,28 @@ export function HomePage (){
                 <ProdcutCard name={"Book 1"} price={12} imageSrc={"./src/Images/aux_book_1.png"} />
                 <ProdcutCard name={"Book 2"} price={25} imageSrc={"./src/Images/aux_book_2.png"} />
                 <ProdcutCard name={"Book 3"} price={34} imageSrc={"./src/Images/aux_book_3.png"} />
+            </section>
+
+            <section className={style.sliderShowContent}>
+                <Slider style={{ display: 'flex', width: '80%', height: '100%' }}
+                    settings={{
+                        dots: true,
+                        infinite: true,
+                        speed: 100,
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    }}
+                >
+                    <div className={style.sliderShowItem}>
+                        <ProdcutCard name={"Book 1"} price={12} imageSrc={"./src/Images/aux_book_1.png"} />
+                    </div>
+                    <div className={style.sliderShowItem}>
+                        <ProdcutCard name={"Book 2"} price={54} imageSrc={"./src/Images/aux_book_2.png"} />
+                    </div>
+                    <div className={style.sliderShowItem}>
+                        <ProdcutCard name={"Book 3"} price={34} imageSrc={"./src/Images/aux_book_3.png"} />
+                    </div>
+                </Slider>
             </section>
 
         </section>
