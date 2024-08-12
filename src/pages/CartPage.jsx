15dbe76f8f90e9cb96a0c76ab4501caaf3ec1756
FeHunter <hunterfe1111@gmail.com>
2024-08-12 18:16:30 -0300

    CartItem...

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,20 +1,47 @@
 import style from "./CartPage.module.css";
 import { Header } from "../components/Header/Header";
 import { CartItem } from "../components/Cart/CartItem/CartItem";
+import { useNavigate } from "react-router-dom";
+import PagesRoutes from "../assets/PagesRoutes";
 
 export function CartPage (){
+
+    const navigate = useNavigate();
+
     return (
         <>
             <Header/>
             <section className={style.CartPage}>
-                <h2>Cart</h2>
+                
                 <div>
+                    <button onClick={()=>{navigate(PagesRoutes.HomePage)}}>Keep buying</button>
+                </div>
+
+                <p>My Cart</p>
+                <div className={style.CartItens}>
                     <CartItem
                         image={"./src/Images/aux_book_1.png"}
-                        name={"Album"}
+                        name={"Album 1"}
                         price={12}
                         amount={1}
                     />
+                    <CartItem
+                        image={"./src/Images/aux_book_2.png"}
+                        name={"Album 2"}
+                        price={24}
+                        amount={1}
+                    />
+                    <CartItem
+                        image={"./src/Images/aux_book_3.png"}
+                        name={"Album 3"}
+                        price={15}
+                        amount={1}
+                    />
+                </div>
+
+                <p>Payment Informations</p>
+                <div>
+                    <button>Go to payment</button>
                 </div>
             </section>
         </>
