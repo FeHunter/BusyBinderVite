986961e705e6ba31878ac9d4a146ff160ebd6122
FeHunter <hunterfe1111@gmail.com>
2024-08-14 17:13:47 -0300

    Show items from loaded api

--- src/components/ProdcutCard/ProdcutCard.jsx
+++ src/components/ProdcutCard/ProdcutCard.jsx
@@ -2,16 +2,21 @@ import { ButtonToBuy } from "../Buttons/Buttons";
 import { ImagesContent } from "../ImagesContent/ImagesContent";
 import style from "./ProdcutCard.module.css";
 
-export function ProdcutCard ({name, price, type, imageSrc}){
+export function ProdcutCard ({item, addToCartFunc}){
+
+    const AddToCart = () => {
+        addToCartFunc(item);
+    }
+
     return (
         <div className={style.prodcutCard}>
-            <ImagesContent src={imageSrc} alt={""} size={'100%'} />
+            <ImagesContent src={item.img} alt={`${item.name}_image`} size={'100%'} />
             <div className={style.prodcutInfos}>
-                <p>{type}</p>
-                <p>{name}</p>
-                <p>${price}</p>
+                <p>{item.type}</p>
+                <p>{item.name}</p>
+                <p>${item.price}</p>
             </div>
-            <ButtonToBuy label={"Buy"} onclick={()=>{}} />
+            <ButtonToBuy label={"Buy"} onclick={()=>{AddToCart()}} />
         </div>
     );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,8 +5,39 @@ import { ImagesContent } from "../components/ImagesContent/ImagesContent";
 import { PageTitle } from "../components/PageTitle/PageTitle";
 import { ProdcutCard } from "../components/ProdcutCard/ProdcutCard";
 import style from "./HomePage.module.css";
+import { useEffect, useState } from "react";
 
 export function HomePage (){
+
+    const [allItems, setAllItems] = useState(null);
+    const [cartItems, setCartItems] = useState(null);
+
+    useEffect(()=>{
+        loadCartItems()
+    },[])
+
+    // Load Itens from firebase - debugging load from local
+    const loadCartItems = () => {
+        fetch("/src/assets/DebugPurpose/RegisterItems.json")
+        .then(response => response.json())
+        .then(json => setAllItems(json));
+    }
+
+    // Add item to the cart
+    const addItemToCart = (item) => {
+        let update = [cartItems];
+        // Item obj
+        const newItem = {
+            id: item.id,
+            type: item.type,
+            name: item.name,
+            price: item.price,
+            img: item.img,
+        }
+        update.push(newItem)
+        setCartItems(update)
+    }
+
     return (
         <>
         <Header/>
@@ -23,11 +54,21 @@ export function HomePage (){
 
             {/* products Highlights */}
             <section className={style.highlightsProducts}>
-                <ProdcutCard type={"Album"} name={"Book 1"} price={12} imageSrc={"./src/Images/aux_book_1.png"} />
-                <ProdcutCard type={"Album"} name={"Book 2"} price={25} imageSrc={"./src/Images/aux_book_2.png"} />
-                <ProdcutCard type={"Album"} name={"Book 3"} price={34} imageSrc={"./src/Images/aux_book_3.png"} />
+                {
+                    allItems ?
+                        allItems.map((item, index) => {
+                            return <ProdcutCard
+                                key={`Product_Card_${index}`}
+                                item={item}
+                                addToCartFunc={()=>{addItemToCart(item)}}
+                            />
+                        })
+                    :
+                    <></>
+                }
             </section>
 
+            {/* Slider Show */}
             <section className={style.sliderShowContent}>
                 <Slider style={{ display: 'flex', width: '80%', height: '100%' }}
                     settings={{
@@ -38,14 +79,17 @@ export function HomePage (){
                         slidesToScroll: 1,
                     }}
                 >
+                    {
+                        
+                    }
                     <div className={style.sliderShowItem}>
-                        <ProdcutCard name={"Book 1"} price={12} imageSrc={"./src/Images/aux_book_1.png"} />
+                        {/* <ProdcutCard name={"Book 1"} price={12} imageSrc={"./src/Images/aux_book_1.png"} /> */}
                     </div>
                     <div className={style.sliderShowItem}>
-                        <ProdcutCard name={"Book 2"} price={54} imageSrc={"./src/Images/aux_book_2.png"} />
+                        {/* <ProdcutCard name={"Book 2"} price={54} imageSrc={"./src/Images/aux_book_2.png"} /> */}
                     </div>
                     <div className={style.sliderShowItem}>
-                        <ProdcutCard name={"Book 3"} price={34} imageSrc={"./src/Images/aux_book_3.png"} />
+                        {/* <ProdcutCard name={"Book 3"} price={34} imageSrc={"./src/Images/aux_book_3.png"} /> */}
                     </div>
                 </Slider>
             </section>
