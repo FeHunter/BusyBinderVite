00a692e6a4bbc1a2398006646a3caf001ff10070
FeHunter <hunterfe1111@gmail.com>
2024-09-02 08:48:18 -0300

    fix: use mailto to send a message

--- src/components/Forms/GetInTouchForm/GetInTouchForm.jsx
+++ src/components/Forms/GetInTouchForm/GetInTouchForm.jsx
@@ -3,21 +3,32 @@ import { ErrorMessage, Field, Form, Formik } from "formik";
 import { ButtonToConfirm } from "../../Buttons/Buttons";
 import * as yup from 'yup'
 
-export function GetInTouchForm ({getValues}){
+export function GetInTouchForm () {
 
     const validationSchema = yup.object({
         name: yup.string().required().min(3),
-        phone: yup.string().required(),
+        phone: yup.number().required(),
         msg: yup.string(),
     })
+
+    const handleSubmit = (values, { resetForm }) => {
+        // Construa a URL mailto
+        const subject = encodeURIComponent("New message from contact form");
+        const body = encodeURIComponent(`Name: ${values.name}\nPhone: ${values.phone}\nMessage: ${values.msg}`);
+        const mailtoURL = `mailto:felipehunter001@gmail.com?subject=${subject}&body=${body}`;
+
+        // Redirecione para a URL mailto
+        window.location.href = mailtoURL;
+
+        // Redefine o formulário após o envio
+        resetForm();
+    }
 
     return (
         <Formik
             initialValues={{ name: '', phone: '', msg: '' }}
             validationSchema={validationSchema}
-            onSubmit={(values)=>{
-                getValues(values)
-            }}
+            onSubmit={handleSubmit}
         >
             <Form className={style.formContent}>
                 <p className={style.pageTitle}>Send me a message</p>
@@ -32,7 +43,7 @@ export function GetInTouchForm ({getValues}){
                     <ErrorMessage name="phone" component="p" className={style.errorMessage} />
                 </div>
                 <div className={style.itemForm}>
-                    <label htmlFor="msg">Mensage*</label>
+                    <label htmlFor="msg">Message*</label>
                     <Field id="msg" name="msg" className={style.fieldInput} />
                     <ErrorMessage name="msg" component="p" className={style.errorMessage} />
                 </div>
